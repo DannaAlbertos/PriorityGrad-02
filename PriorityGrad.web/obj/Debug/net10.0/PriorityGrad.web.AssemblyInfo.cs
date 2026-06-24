daa9b387f39698c88759f0fdfa7709a3cba362fe
DannaAlbertos <danna.albertos@tecdesoftware.edu.mx>
2026-06-24 08:29:15 -0600

    Link en la barra de navegacion

--- PriorityGrad.web/obj/Debug/net10.0/PriorityGrad.web.AssemblyInfo.cs
+++ PriorityGrad.web/obj/Debug/net10.0/PriorityGrad.web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PriorityGrad.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f6789bc13587156d6f3498da0792a3932105fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PriorityGrad.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PriorityGrad.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
